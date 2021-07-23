0995f4ed1b4e8410ed2e874005d68d4f725d4b27
Colin Peters <colinthedev@gmail.com>
2021-07-23 11:38:01 -0600

    finished animations all pages

--- animations.js
+++ animations.js
@@ -1,57 +1,52 @@
 'use strict';
 gsap.registerPlugin(ScrollTrigger);
 
-// // Hero Sections
-//     window.onload = function() {
-//         if (window.location.href.indexOf('stories.html') > -1) {
-//         // Hero sec stories
-//             gsap.to('.storiesAnimation', {ease: "sine.out", x:0, opacity: 1, duration: 2, delay: .5});
-//         } else {
-//         // Hero sec index, features, pricing
-//             gsap.to('.navInviteBtn', {ease: "back.out(1.8)", x:0, opacity: 1, duration: 2});
-//             gsap.to('.topImgContainer', {ease: "circ.out", x:0, opacity: 1, duration: 1.75, delay: 1.10});
-//             gsap.to('.topTxtContainer', {ease: "circ.out", x:0, opacity: 1, duration: 1.75, delay: 1.15});
-//             gsap.to('.rainbowAnimation', {ease: "circ.out", opacity: 1, duration: 2, delay: 3.10});
-        
-//             let heroSection = gsap.timeline ({ defaults: {x: 0, opacity: 1, duration: 2, delay: 3.05, ease: "circ.out", }})
-//                 .to('.headingXLargeWht', {})
-//                 .to('.summaryWhite', {}, "-=4.35")
-//                 .to('.rowSml', {}, "-=4.35");
-//             return heroSection;
-//         }
-//     }
+// Prevent useless warnings in console if class is not found
+    gsap.config({
+        nullTargetWarn: false,
+    });
+
+    // gsap.defaults({
+    //     opacity: 0, 
+    // })
 
 // Hero Sections
-window.onload = function() {
-    if (window.location.href.indexOf('stories.html') > -1) {
-    // Hero sec stories
-        gsap.from('.storiesAnimation', {ease: "sine.out", x: 150, opacity: 0, duration: 2, delay: .5});
-    } else {
-    // Hero sec index, features, pricing
-        gsap.from('.navInviteBtn', {ease: "back.out(1.8)", x: 200, opacity: 0, duration: 2});
-        gsap.from('.topImgContainer', {ease: "circ.out", x: 1000, opacity: 0, duration: 1.75, delay: 1.10});
-        gsap.from('.topTxtContainer', {ease: "circ.out", x: -1000, opacity: 0, duration: 1.75, delay: 1.15});
-        gsap.from('.rainbowAnimation', {ease: "circ.out", opacity: 0, duration: 2, delay: 3.10});
-    
-        let heroSection = gsap.timeline ({ defaults: {x: 150, opacity: 0, duration: 2, delay: 3.05, ease: "circ.out", }})
-            .from('.headingXLargeWht', {})
-            .from('.summaryWhite', {}, "-=4.35")
-            .from('.rowSml', {}, "-=4.35");
-        return heroSection;
-    }
-}
+    window.onload = function() {
+        if (window.location.href.indexOf('stories.html') > -1) {
+        // Stories
+            gsap.from('.storiesAnimation', {x: 150, opacity: 0, duration: 2, delay: .5, ease: "sine.out"});
+            gsap.from('.navInviteBtn', {x: 200, opacity: 0, duration: 2, ease: "back.out(1.8)"});
+
+        } else {
+        // Index, features, pricing
+            gsap.from('.navInviteBtn', {x: 200, opacity: 0, duration: 2, ease: "back.out(1.8)"});
+            gsap.from('.topImgContainer', {x: 1000, opacity: 0, duration: 1.75, delay: 1.10, ease: "circ.out"});
+            gsap.from('.topTxtContainer', {x: -1000, opacity: 0, duration: 1.75, delay: 1.15, ease: "circ.out"});
+            gsap.from('.rainbowAnimation', {opacity: 0, duration: 2, delay: 3.10, ease: "circ.out"});
+        
+            let heroSection = gsap.timeline ({ defaults: {x: 150, opacity: 0, duration: 2, delay: 3.05, ease: "circ.out", }})
+                .from('.headingXLargeWht', {})
+                .from('.ctaAnimation', {}, "-=4.35")
+                .from('.rowSml', {}, "-=4.35");
+            return heroSection;
+        };
+    };
 
 // Index stories images
+    let imgContainerDefaults = { // Default values for "imgContainerBeautiful" , "imgContainerDesigned"
+        opacity: 0,
+        duration: 1.85,
+        ease: "power1.out"
+    }
+
     gsap.from('.imgContainerBeautiful', {
         scrollTrigger: {
             trigger: '.imgContainerBeautiful',
             start: "top center"
         },
         x: -1000,
-        opacity: 0,
-        duration: 1.85,
-        ease: "power1.out"
-    })
+        ...imgContainerDefaults
+    });
 
     gsap.from('.imgContainerDesigned', {
         scrollTrigger: {
@@ -59,23 +54,74 @@ window.onload = function() {
             start: "top center"
         },
         x: 1000,
-        opacity: 0,
-        duration: 1.85,
-        ease: "power1.out"
-    })
-
-// Index svg images
-    let indexSvgTl = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.indexSvgRowContainer',
-            start: "center bottom"
-        }
+        ...imgContainerDefaults
     });
 
+// Stories
+    let storiesDefaults = { // Default values for "storiesTl"
+        y: -75, 
+        opacity: 0, 
+        ease: "sine.out",
+        duration: .75
+    }
+
+    let storiesTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.topImgContainerFull',
+            start: 'center center'
+        },
+    })
+
+    storiesTl
+        .from('.storiesMountainsContainer', {...storiesDefaults})
+        .from('.storiesCityScapeContainer', {...storiesDefaults}, "+=.10")
+        .from('.storiesDaysVoyageContainer', {...storiesDefaults}, "-=.90")
+        .from('.storiesArchitecturalsContainer', {...storiesDefaults}, "+=.10")
+
+
+// Index + Features svg images
+    let indexSvgDefaults = { // Default values for "indexSvgTl"
+        y: 100, 
+        opacity: 0, 
+        ease: "sine.out",
+        duration: 1.05
+    }
+
+    let indexSvgTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.contentWrapperSVG',
+            start: "top bottom"
+        },
+    });
+ 
     indexSvgTl
-        .from('.responsiveContainer', {y: 100, opacity: 0, ease: "sine.out", duration: 1.5})
-        .from('.photoUploadContainer', {y: 100, opacity: 0, ease: "sine.out", duration: 1.5}, "-=.75")
-        .from('.embedContainer', {y: 100, opacity: 0, ease: "sine.out", duration: 1.5}, "-=.75")
+        .from('.responsiveContainer', {...indexSvgDefaults})
+        .from('.photoUploadContainer', {...indexSvgDefaults}, "-=.10")
+        .from('.embedContainer', {...indexSvgDefaults}, "-=.20")
+        .from('.domainContainer', {...indexSvgDefaults}, "+=0")
+        .from('.boostExposureContainer', {...indexSvgDefaults}, "-=.10")
+        .from('.dragDropContainer', {...indexSvgDefaults}, "-=.20");
+
+// Pricing 
+    let pricingDefaults = { // Default values for "pricingTl"
+        y: 100, 
+        opacity: 0, 
+        ease: "sine.out",
+        duration: 1.35
+    }
+
+    let pricingTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.basicPricePlanContainer',
+            start: "center bottom"
+        },
+    });
+
+    pricingTl
+        .from('.basicPricePlanContainer', {...pricingDefaults})
+        .from('.proPricePlanContainer', {...pricingDefaults}, "+=.15")
+        .from('.businessPricePlanContainer', {...pricingDefaults}, "-=2.15")
+        .from('.slideLeft', {x: 400, opacity: 0, ease: "sine.out"}, "+=.05");
 
 
 
